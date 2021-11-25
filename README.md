b508af95a5f187a799257232e94011311b60fe5d
kseniaglivko <k.glivko@mail.ru>
2021-11-25 22:09:26 +0300

    Project reorganized, updated README.

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
-# Secret_Santa_Telegram_bot
-Telegram Bot to dynamically assign Secret Santas for a specific list of participants. 
-Language: Russian
+# Secret Santa Telegram bot
+This is a telegram bot to dynamically assign Secret Santas to a specific list of participants 
 
-This Bot is not universal - it works with a specific list of participants.
+**Note**: the bot was created to be used with russian names. 
 
-1.  User types in his/her name, then the names is checked to be in the participants list. If it is not in the list - bot gives an error.
-2. The name is checked to be in the database - to check whether the person already has an asignee. If the person enters his/her name twice - bot gives an error.
-3. After checking, bot assigns a name from the participants list and uploads information about Santa and the asignee to the database so the same name wouldn't be assigned twice or the person wouldn't be assigned to his/herself.
+Bot is case insensitive and understands diminutive names.
 
-Bot is case insensitive. Bot takes diminutive names.
+
+
+## Description
+
+1.  User types in his/her name, then the name is checked to be in the participants list. If it is not in the list - bot throws an error.
+2. Then the name is checked to be in the database - to check whether the user already has an asignee. If the person enters his/her name twice - bot throws an error. 
+3. After checking, bot assigns a name from the participants list to the user, displays the name and a sticker to the user. While assigning, bot excludes the user's name from selection.
+4. Bot uploads information about Santa and his/hers asignee to the database so the same person wouldn't be assigned twice.
+
+
+## Installation
+
+1. You need to create a telegram bot and get API Token. See more information here: https://core.telegram.org/bots
+
+2. ```git clone https://github.com/kseniaglivko/Secret_Santa_Telegram_bot.git```
+
+3. Open the project in IDE and set up virtual environment.
+
+4. ```pip install -r requirements.txt```
+
+5. Insert your API token in ```bot``` variable in bot.py line 7.
+
+6. Before using it you may want to update names in ```participants_surnames``` variable in utils.py and in ```participants``` variable in bot.py, modify ```get_declensed_name``` function in utils.py and so on. You are welcome to modify the code for your own needs.
+
+7. Go to project directory, open terminal and run ```python3 main.py```
+
+Bot will be active while the terminal is running.
+
+
+## Tests
+
+"tests" directory contains some unittests you may run to check if bot works properly.
--- utils.py
+++ utils.py
@@ -1,49 +1,49 @@
 participants_surnames = {
     "гливко": "ксения гливко",
-	"чернигин": "илья чернигин",
-	"благодарный": "илья благодарный",
-	"терентьев": "антон терентьев",
-	"лущаева": "ангелина лущаева",
-	"алексеева": "александра алексеева",
-	"кислов": "геннадий кислов",
-	"усольцева": "ася усольцева",
-	"куликова": "мария куликова",
-	"панасюк": "александр панасюк",
-    }
+    "чернигин": "илья чернигин",
+    "благодарный": "илья благодарный",
+    "терентьев": "антон терентьев",
+    "лущаева": "ангелина лущаева",
+    "алексеева": "александра алексеева",
+    "кислов": "геннадий кислов",
+    "усольцева": "ася усольцева",
+    "куликова": "мария куликова",
+    "панасюк": "александр панасюк",
+}
 
 
 def get_declensed_name(fullname):
     """Returns denclensed name for the final message with the info about Secret Santa's appointee"""
-    lst = fullname.split(' ')
+    lst = fullname.split(" ")
     name = lst[0]
     surname = lst[1]
     last_char_name = lst[0][-1]
     last_char_surname = lst[1][-1]
-    if last_char_name == 'я':
-        if last_char_surname == 'н':
-            lst = name[:-1] + 'и ' + surname + 'а'
-        elif last_char_surname == 'й':
-            lst = name[:-1] + 'и ' + surname[:-2] + 'ого'
-        elif last_char_surname == 'а':
-            lst = name[:-1] + 'и ' + surname[:-1] + 'oй'
+    if last_char_name == "я":
+        if last_char_surname == "н":
+            lst = name[:-1] + "и " + surname + "а"
+        elif last_char_surname == "й":
+            lst = name[:-1] + "и " + surname[:-2] + "ого"
+        elif last_char_surname == "а":
+            lst = name[:-1] + "и " + surname[:-1] + "oй"
         else:
-            lst = name[:-1] + 'и ' + surname
-    elif last_char_name and last_char_surname == 'а':
-        lst = name[:-1] + 'ы ' + surname[:-1] + 'oй'
-    elif last_char_surname == 'в':
-        if last_char_name == 'н':
-            lst = name + 'a ' + surname + 'a'
-        elif last_char_name == 'й':
-            lst = name[:-1] + 'я '+ surname + 'a'
-    elif last_char_surname == 'к':
-        lst = name + 'a ' + surname + 'a'
+            lst = name[:-1] + "и " + surname
+    elif last_char_name and last_char_surname == "а":
+        lst = name[:-1] + "ы " + surname[:-1] + "oй"
+    elif last_char_surname == "в":
+        if last_char_name == "н":
+            lst = name + "a " + surname + "a"
+        elif last_char_name == "й":
+            lst = name[:-1] + "я " + surname + "a"
+    elif last_char_surname == "к":
+        lst = name + "a " + surname + "a"
     return str(lst)
 
 
 def get_proper_name(message):
     """Returns the full form of the name to enable the use of diminutive names and such"""
     try:
-        fullname = message.split(' ')
+        fullname = message.split(" ")
     except AttributeError:
         return "некорректное имя"
     else:
